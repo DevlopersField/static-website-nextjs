62b4f86d85e7d27501a260e34689432dc4c81eb7
virajChuaudhary <virajdev3052003@gmail.com>
2025-04-28 21:12:02 +0530

    upload new

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,9 @@ export default function About() {
     <div className="text-center ">
       <Navbar />
       <h1 className="text-4xl font-bold mb-4">About Me</h1>
-      <p className="text-lg">I'm learning Next.js, TypeScript, and Tailwind CSS to build awesome projects!</p>
+      <p className="text-lg">
+        I&apos;m learning Next.js, TypeScript, and Tailwind CSS to build awesome projects!
+      </p>
     </div>
   );
 }
